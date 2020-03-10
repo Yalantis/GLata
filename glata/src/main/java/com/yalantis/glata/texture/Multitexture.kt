ee4bf6cc9289e3c600f05f3d4a3f34d8d96cbb58
ilia.solodiankin <ilia.solodiankin@yalantis.com>
2020-03-10 02:46:02 +0200

    GLata update

--- glata/src/main/java/com/yalantis/glata/texture/Multitexture.kt
+++ glata/src/main/java/com/yalantis/glata/texture/Multitexture.kt
@@ -4,18 +4,20 @@ import android.opengl.GLES20
 import com.yalantis.glata.core.RendererParams
 import com.yalantis.glata.core.texture.ITexture
 
-class Multitexture(private val textures: List<ITexture>) : ITexture {
+class Multitexture(private val name: String, private val textures: List<ITexture>) : ITexture {
 
-    override fun createTexture(rp: RendererParams) {
+    override fun createTexture(rendererParams: RendererParams) {
         for (texture in textures) {
-            texture.createTexture(rp)
+            texture.createTexture(rendererParams)
         }
     }
 
-    override fun bind(rp: RendererParams) {
+    override fun getName(): String = name
+
+    override fun bind(rendererParams: RendererParams) {
         for (i in textures.indices) {
             GLES20.glActiveTexture(GLES20.GL_TEXTURE0 + i)
-            textures[i].bind(rp)
+            textures[i].bind(rendererParams)
         }
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
     }
